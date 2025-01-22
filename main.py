a98279ef9575f7ecdeb4d86124932e2b4c827415
Adam Maxwell <maxwell7774@gmail.com>
2025-01-22 00:45:41 -0700

    Adds the ability to draw lines

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from line import Line
+from point import Point
 from window import Window
 
 
 def main():
     win = Window(800, 600)
+    line_1 = Line(Point(20, 20), Point(100, 100))
+    line_2 = Line(Point(40, 140), Point(100, 100))
+    win.draw_line(line_1, "black")
+    win.draw_line(line_2, "red")
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -1,9 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
+from line import Line
+
 
 class Window:
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.canvas = Canvas(width=width, height=height)
@@ -12,14 +14,18 @@ class Window:
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self.running = True
         while self.running:
             self.redraw()
 
-    def close(self):
+    def close(self) -> None:
         self.running = False
+
+    def draw_line(self, line: Line, fill_color: str) -> None:
+        line.draw(self.canvas, fill_color)
+
